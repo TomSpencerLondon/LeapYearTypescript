665d366d0b903357412c6eca99b24f4b24c6aefc
Tom <thomas.spencer@codurance.com>
2020-11-02 21:24:08 +0000

    Passing test for leap year if divisible by 400

--- src/leap-year.ts
+++ src/leap-year.ts
@@ -1,3 +1,8 @@
-export const leapYear = (input: number): boolean => {
+export const leapYear = (year: number): boolean => {
+  if (year % 4 !== 0){
+    return false;
+  }else if (year % 400 === 0) {
+    return true;
+  }
   return false;
 };
--- test/leap-year.test.ts
+++ test/leap-year.test.ts
@@ -2,9 +2,13 @@ import {leapYear} from "../src/leap-year";
 
 describe("Leap Year should", () => {
   it.each([
-    [1997, false]
+    [1997, false],
+    [1999, false]
   ])("not be a leap year if not divisible by 4", (year: number, result: boolean) => {
     expect(leapYear(year)).toEqual(result);
   })
 
+  it("be a leap year if divisible by 400", () => {
+    expect(leapYear(1600)).toEqual(true);
+  })
 })
